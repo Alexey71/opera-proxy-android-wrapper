58138b1cc7f08303863f34561a8fde46915ce0c7
SLY-F0X <271353452+SLY-F0X@users.noreply.github.com>
2026-03-27 00:16:49 +0300

    Update binary components base, more connects to proxy allowed, Update LICENSE TERMS Remove shutdown server for DoH
    НЕ для коммерчесекого использования, собирания пожертвовани, донатов!
    Подпись приложения всегда новая, сборка через GitHub Actions, оно не встанет обновлением на существующую установку!
    Как и старые версии приложения.
    
    Репозиторий был удален, так как НЕТ никакого желания дарить подарки скаммерам в виде Киберпортала и Portal Connect!
    Все Issue о подозрительном поведении их ПО быстро подтираются.

--- app/src/main/java/com/example/operaproxy/AdvancedSettingsActivity.kt
+++ app/src/main/java/com/example/operaproxy/AdvancedSettingsActivity.kt
@@ -52,9 +52,9 @@ class AdvancedSettingsActivity : AppCompatActivity() {
     // Значения по умолчанию
     private val DEFAULT_BIND = "127.0.0.1:1085"
     private val DEFAULT_BOOTSTRAP =
-        "https://1.1.1.3/dns-query,https://8.8.8.8/dns-query,https://dns.google/dns-query,tls://9.9.9.9:853,https://security.cloudflare-dns.com/dns-query,https://fidelity.vm-0.com/q,https://wikimedia-dns.org/dns-query,https://dns.adguard-dns.com/dns-query,https://dns.quad9.net/dns-query,https://dns.comss.one/dns-query,https://router.comss.one/dns-query"
+        "https://1.1.1.3/dns-query,https://8.8.4.4/dns-query,tls://9.9.9.9:853,https://security.cloudflare-dns.com/dns-query,https://wikimedia-dns.org/dns-query,https://dns.adguard-dns.com/dns-query,https://dns.quad9.net/dns-query,https://dns.comss.one/dns-query,https://router.comss.one/dns-query"
     private val DEFAULT_TEST_URL =
-        "https://ajax.googleapis.com/ajax/libs/indefinite-observable/2.0.1/indefinite-observable.bundle.js"
+        "https://translate.yandex.ru/icons/favicon.ico"
     private val DEFAULT_VERBOSITY_INDEX = 2 // 20 Info
     private val DEFAULT_DNS_STRATEGY_INDEX = 1 // 1 = OverTcp (Default)
 
